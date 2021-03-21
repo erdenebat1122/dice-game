3dd5eeca78389295eb84482ed6bdc658aa881018
erdenebat <erdenebatbld@gmail.com>
2021-03-22 05:00:13 +0800

    Shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -9,15 +9,20 @@ var scores = [0, 0];
 var roundScore = 0;
 
 // Шооны аль талаараа буусныг хадгалах хувьсагч, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-// var dice = Math.floor(Math.random()*6) + 1;
+var diceNumber = Math.floor(Math.random()*6)+1;
+// Программ эхлэхэд бэлтгэе
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
-// <div class="player-score" id="score-0">43</div>
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
+document.querySelector(".btn-roll").addEventListener("click",function(){
+    var diceNumber = Math.floor(Math.random()*6)+1;
 
+    diceDom.style.display = "block";
+    diceDom.src = 'dice-' + diceNumber + '.png';
 
-document.querySelector(".dice").style.display = "none";
-console.log('Шоо :  ' + dice );
+});

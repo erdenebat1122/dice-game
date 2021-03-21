24f50e5e3cfea8b0521f8ec2edbc51eceaf744d8
erdenebat <erdenebatbld@gmail.com>
2021-03-22 06:09:03 +0800

    Shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Тоглогчийн ээлжийг хадгалах хувьсагч, Player 1= 0, Player 2=1 гэж тмдглнэ
-var activePlayer = 1;
+var activePlayer = 0;
 
 // Тоглогчдын цуглуулсан оноог хадгалах
 var scores = [0, 0];
@@ -18,11 +18,18 @@ document.getElementById('current-1').textContent = '0';
 
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
-
+// Шоог шидэх Eventlistener
 document.querySelector(".btn-roll").addEventListener("click",function(){
+    // 1- 6 хүртэл санамсаргүй 1 тоог гаргаж авна
     var diceNumber = Math.floor(Math.random()*6)+1;
-
+    // шооны зургыг web дээр гаргаж ирнэ
     diceDom.style.display = "block";
+    // Буусан санамсаргүй тоонд харгалзах шооны зургыг web дээр гаргаж ирнэ
     diceDom.src = 'dice-' + diceNumber + '.png';
-
+    // тоглогчын ээлжийн оноог өөрчилнө.
+    if(diceNumber !== 1){
+//1 ээс ялгаатай тоо буулаа.Нэмнэ
+    }else{
+// 1 буусан тул тоглогчийн ээлжийг энэн хэсэгт сольж өгнө
+    }
 });
